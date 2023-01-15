26c3391073e1e24db460f7c239949830a88d9b06
mlorber <87738509+mlorberdev@users.noreply.github.com>
2023-01-14 19:39:38 -0500

    update

--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,5 @@
 !(function () {
 
-  // setTimeout(() => {
-  //   document.getElementById("loader").style.display = "none";
-  // },1000);
-
   // CLOCK TIME
   const time = () => {
     let d = new Date().toLocaleTimeString().split(":");
@@ -15,139 +11,113 @@
   const txt = document.querySelectorAll(".txt");
   const sms = document.getElementById("sms-marker");
   const texts = [
-    `Hi! 👋 It's Matt`,
-    `How are you 🤔❔`,
-    `Want to play a game? 👾`,
-    ` <span><a href="https://wrdl-with-restful-api.netlify.app/">Wordle<sup>TM</sup> Clone</a>` +
-    `🔗</span><span><a href="https://minesweeper-clone-jquery.netlify.app/">Minesweeper</a> 🔗</span>` +
-    `<span><a href="https://snake-vanilla-js-canvas.netlify.app/">Snake</a> 🔗❔</span>`,
-    `Make a <span><a href="https://qr-code-styler.netlify.app/">QR code</a> 🔗 for your WiFi/Event/etc...?`,
-    `Or grab a free <span><a href="https://mlorberdev-generative-art.netlify.app/">digital art masterpiece?</a> 🔗 Maybe for a background ...`,
-    `actually, try <a href="https://particles-background.netlify.app/">this experiment</a> for bgs...🕶️`,
-    `You can check out these and my other work on <a href="https://github.com/mlorberdev">Github</a> 😎` +
-    ` and <a href="https://codepen.io/mlorberdev">Codepen</a> 🕶️`,
-    `...find me on <a href="https://www.linkedin.com/in/matthew-lorber">LinkedIn</a>`,
-    `Have a quick look at my <a href="./stats.html">stats</a> page while you're here?`,
+    `Hi👋 It's Matt<br>How are you 🤔`,
+    `Want to play a game? 🕹️👾🎮`,
+    `<span><a href="https://wrdl-with-restful-api.netlify.app/" target="_blank">Wordle<sup>TM</sup> Clone </a></span><br>` +
+    `<span><a href="https://minesweeper-clone-jquery.netlify.app/" target="_blank">Minesweeper</a></span><br>` +
+    `<span><a href="https://snake-vanilla-js-canvas.netlify.app/" target="_blank">Snake</a> 🔗❔</span>`,
+    `Make a <span><a href="https://qr-code-styler.netlify.app/" target="_blank">QR code</a> for your WiFi/Event/etc...?`,
+    `Or grab a free <span><a href="https://mlorberdev-generative-art.netlify.app/" target="_blank">digital art masterpiece?</a> Maybe for a background ...`,
+    `You can check out these &mdash; and my other work &mdash; on <a href="https://github.com/mlorberdev" target="_blank">Github</a>` +
+    ` and <a href="https://codepen.io/mlorberdev" target="_blank">Codepen</a> 😎`,
+    `...find me on <a href="https://www.linkedin.com/in/matthew-lorber" target="_blank">LinkedIn</a>...`,
+    `...and have a quick look at my <a href="./stats.html" target="_blank">stats</a> page while you're here?`,
     `<a href="mailto:mlorber.dev@gmail.com">Email</a> 🫱‍🫲🏾 me if you'd like to chat!`,
-    `Glad you stopped by👍!! Let's get together soon! 💯`
+    `Glad you stopped by 💯! Let's get together on something soon!`
   ];
   const replies = [
     `*What's a xxxxx? Is that like a 🧌❔`,
     `*xxxxx 🎉`,
     `*xxxxx seriously❔`,
     `*xxxxx's ⭐⭐⭐`,
-    `*xxxxx! I'm not really an AI 😁 I have no idea what you're saying 🤪`,
-    `*<a href="https://loveyouineke.netlify.app/">Game of Life</a>`
+    `*xxxxx! I'm not really an AI 😁 I have no idea what you're saying 🤪`
+    // `*<a href="https://loveyouineke.netlify.app/" target="_blank">Game of Life</a>`
   ];
 
-  // TIMING FUNCTION
-  const int = setInterval(() => {
-    message();
-    if (texts.length === 0) {
-      sms.style.display = "none";
-      clearInterval(int);
-      setTimeout(() => document.querySelectorAll(".now:last-of-type").innerHTML = time(), 5000);
-    }
-  }, 3000);
+  // TIMING
+
+  const int = setInterval(message, 8000);
 
   // TEXTS SENT BY MY AVATAR
   function message() {
+    sms.style.display = "block";
     let msg = document.createElement("div");
     let txt = texts[0]; // note: replies get spliced into texts array
     msg.innerHTML = `<svg width="3rem" height="3rem" viewBox="0 0 512 512"><use xlink:href="#avatar" /></svg><span class="txt">${txt}</span>`;
-    msg.classList.add("msg", "flex", "row");
+    msg.classList.add("msg", "flex", "row", "xmsg");
     msg.id = `msg${N}`;
-    msg.addEventListener("click", toggleNow);
     if (txt.charAt(0) === "*") msg.innerHTML = msg.innerHTML.replace("*", "");
     let now = document.createElement("div");
     now.classList.add("now");
     now.id = `now${N}`;
-    N++;
     now.innerHTML = `Now`;
     sms.style.display = "flex";
     texts.splice(0, 1);
-    screen.appendChild(msg);
-    screen.appendChild(now);
-    removeNowQuestionMark();
-    // screen.scrollTop = screen.scrollHeight;
-    // setTimeout(() => {
-    //   sms.style.display = "none";
-    //   setTimeout(() => {
-    //     screen.append(msg);
-    //     screen.append(now);
-    //     screen.scrollTop = screen.scrollHeight;
-    //     if (texts.length > 0) {
-    //       document.querySelectorAll(".now:last-of-type")[0].style.display = "none";
-    //     }
-    //     screen.scrollTop = screen.scrollHeight;
-    //     setTimeout(() => now.style.visibility = "visible", 1000);
-    //   }, 500);
-    //   setTimeout(() => { now.innerHTML = time() }, 10000);
-    // }, 2200);
+    setTimeout(() => {
+      sms.style.display = "none";
+    }, 3000);
+    setTimeout(() => {
+      removeNowQuestionMark(); // remove prior timestamp
+      screen.appendChild(msg);
+      screen.appendChild(now);
+      N++;
+      br(); // border radius
+      screen.scrollTop = screen.scrollHeight;
+    }, 3300);
+    if (texts.length === 0) {
+      clearInterval(int);
+      setTimeout(() => document.querySelector(".now:last-of-type").innerHTML = time(), 15000);
+    }
   }
 
   // SPLICE A RESPONSE INTO AVATAR TEXTS ARRAY IN RESPONSE to USER MESSAGE input event
   function reply(x) {
+    if (replies.length === 0) return;
     setTimeout(() => {
-      replies[0] = replies[0].toString().replace("xxxxx", x)
+      replies[0] = replies[0].toString().replace("xxxxx", x);
+      if (texts.length === 0) setTimeout(message, 2500);
       texts.splice(0, 0, replies[0]);
       replies.splice(0, 1);
     }, 2000);
   }
 
   // USER MESSAGE input event
-  let numUserMessages = 0;
-  // let sendPrompt;
   document.getElementById("chat").addEventListener("change", function () {
-    numUserMessages++; // if user doesn't respond after a number of messages, send a prompt
-    console.log(numUserMessages);
-
-
-    // prompt logic here (ie if numusermessages is still zero, then...)
-
-    // splice new message in
-    // possibly type it into field
-    // this could get crazy...
-
     let mm = this.value;
     let mms = mm.split(" ")[Math.floor(Math.random() * (mm.split(" ").length))]; // random word from user msg recieved
     let um = document.createElement("div"); // user message
-    um.classList.add("user_input_wrap", "flex", "row", "right");
+    um.classList.add("user_input_wrap", "flex", "row", "right", "xmsg"); // xmsg tracks how many texts of any type
     um.innerHTML = `<div class="user_input">${mm}</div>`;
     this.value = "";
-
     let now = document.createElement("div");
     now.classList.add("now", "left");
-    now.id = `now${N}`;
+    now.id = `msg${N}`;
     N++;
     now.innerHTML = `Now`;
     screen.appendChild(um);
     screen.appendChild(now);
+    br();
     removeNowQuestionMark();
-    // screen.scrollIntoView(um);
     reply(mms);
-    // screen.scrollTop = screen.scrollHeight;
+    screen.scrollTop = screen.scrollHeight;
   });
 
   // BORDER RADIUS MODS
-  function br(i, j) {
-    console.log(i, j);
-    txt[i - 2].style.borderBottomLeftRadius = 0;
-    txt[i - 1].style.borderTopLeftRadius = 0;
-    if (j) txt[i - 1].style.borderTopLeftRadius = 0;
-  }
-
-  // HIDING AVATAR but KEEP FOR SPACING
-  function ha(i) {
-    document.querySelector(`.msg:nth-of-type(${i}) svg`).style.visibility = "hidden";
+  function br() {
+    if (N === 1) return;
+    let msgs = document.querySelectorAll(".xmsg");
+    
+    // txt[i - 2].style.borderBottomLeftRadius = 0;
+    // txt[i - 1].style.borderTopLeftRadius = 0;
+    // if (j) txt[i - 1].style.borderTopLeftRadius = 0;
   }
 
   // ADD SPACE AFTER LONGER PAUSES IN ANIM SEQUENCE
-  function space(n) {
-    let sp = document.createElement("div");
-    sp.classList.add("space");
-    // insert it at end
-  }
+  // function space(n) {
+  //   let sp = document.createElement("div");
+  //   sp.classList.add("space");
+  //   // insert it at end
+  // }
 
   // EMOJIS IN TEXT INPUT
   document.getElementById("icons").addEventListener("click", function () {
@@ -165,37 +135,11 @@
   setTime();
   setInterval(setTime, 60000);
 
-  // SCROLL STATE
-  const down = document.getElementById("screen");
-  void function upDown() {
-    const observer = new IntersectionObserver(entries => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          console.log("int");
-        }
-      });
-    }, { threshold: 1 });
-    observer.observe(down);
-  }();
-
-
-  // // ANIMATION RUN/PAUSE USING INTERSECTION OBSERVER
-  // void function pauseAnim() {
-  //   const observer = new IntersectionObserver(entries => {
-  //     entries.forEach(entry => entry.target.style.animationPlayState = entry.isIntersecting ? "running" : "paused");
-  //   }, { threshold: 1 });
-  //   observer.observe(down)
-  // cows.forEach(cow => observer.observe(cow));
-  // }();
-
   // HIDE && SHOW TIMESTAMPS
   function removeNowQuestionMark() {
     let allNows = document.querySelectorAll(".now");
-    let l = allNows.length - 1;
-    if (l > 0) allNows[l - 1].style.display = "none";
-  }
-  function toggleNow() {
-    document.getElementById(`now${this.id.toString().split("g")[1]}`).classList.toggle("hide");
+    let l = allNows.length;
+    if (l > 0) allNows[l-1].style.display = "none";
   }
 
 })();
