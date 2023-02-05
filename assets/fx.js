25f6a5b704f02877240c78ba1d9820fcbf85dae1
mlorber <87738509+mlorberdev@users.noreply.github.com>
2023-02-05 01:00:12 -0500

    updates for firefox

--- assets/fx.js
+++ assets/fx.js
@@ -13,14 +13,16 @@
 // }();
 
 // STATS PAGE SCROLL
-document.getElementById("down_wrap").addEventListener("click", function () {
-  let t = this.style.transform;
-  let sw = document.getElementById("stats_wrap");
-  if (t === "") {
-    sw.scrollTop = sw.scrollHeight;
-    this.style.transform = "rotate(180deg)", 200;
-  } else {
-    sw.scrollTop = 0;
-    this.style.transform = "", 200;
-  }
-});
+!(function () {
+  document.getElementById("down_wrap").addEventListener("click", function () {
+    let t = this.style.transform;
+    let sw = document.getElementById("stats_wrap");
+    if (t === "") {
+      sw.scrollTop = sw.scrollHeight;
+      this.style.transform = "rotate(180deg)", 200;
+    } else {
+      sw.scrollTop = 0;
+      this.style.transform = "", 200;
+    }
+  });
+})();
